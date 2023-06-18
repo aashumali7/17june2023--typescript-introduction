8ded07274d9d5c0c531c538873d928598f381139
aashumali7 <aashishmali223@gmail.com>
2023-06-18 12:27:52 +0530

    new

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+var name1 = "aashu"; //declaration and initialization
+console.log(name1);
+var age; //delaration 
+age = 19;
+console.log(age);
---
+++ js/custom.ts
@@ -0,0 +1,11 @@
+
+
+var name1:string="aashu"; //declaration and initialization
+
+console.log(name1);
+
+var age:number; //delaration 
+
+age= 19
+
+console.log(age);
